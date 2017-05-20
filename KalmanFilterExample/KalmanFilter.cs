383230117b672051a2e7be2aacaa94fd18448848
Giseok Kim <ladofa@live.co.kr>
2017-05-20 20:24:13 +0900

    대충 마무리

--- KalmanFilterExample/KalmanFilter.cs
+++ KalmanFilterExample/KalmanFilter.cs
@@ -22,7 +22,9 @@ namespace KalmanFilterExample
 		//time interval
 		float dt;
 
-		public void Init(float xp, float xv, float p, float dt)
+		float r;
+
+		public void Init(float xp, float xv, float p, float r, float dt)
 		{
 			this.xp = xp;
 			this.xv = xv;
@@ -30,25 +32,65 @@ namespace KalmanFilterExample
 			this.p2 = 0;
 			this.p3 = 0;
 			this.p4 = p;
-			
+			this.r = r;
 		}
 
-		public void Update(float z)
+		public float Update(float z)
 		{
 			//------------------------------------
 			//predict
 			xp = xp + dt * xv;
 
-			float q1 = p1 + dt * p3 + dt * p2 + dt * dt * p4;
-			float q2 = 
-
-			//xv = xv;
-
-
-			
+			float q1 = p1 + dt * (p2 + p3) + dt * dt * p4;
+			float q2 = p2 + dt * p4;
+			float q3 = p3 + dt * p4;
 
 			//--------------------------------------
 			//correct
+			float y = z - xp;
+			float k1 = q1 / (q1 + r);
+			float k2 = q3 / (q1 + r);
+
+			xp = xp + k1 * y;
+			xv = xv + k2 * y;
+
+			p1 = k1 * q1;
+			p2 = k1 * q2;
+			p3 = k2 * q1;
+			p4 = k2 * p2;
+
+			return xp;
+		}
+
+		/// <summary>
+		/// 아무 입력 없이 업데이트
+		/// </summary>
+		/// <returns></returns>
+		public float Update()
+		{
+			//------------------------------------
+			//predict
+			xp = xp + dt * xv;
+
+			float q1 = p1 + dt * (p2 + p3) + dt * dt * p4;
+			float q2 = p2 + dt * p4;
+			float q3 = p3 + dt * p4;
+
+			//--------------------------------------
+			//correct
+			float y = 0;
+			float k1 = q1 / (q1 + r);
+			float k2 = q3 / (q1 + r);
+
+			xp = xp + k1 * y;
+			xv = xv + k2 * y;
+
+			p1 = k1 * q1;
+			p2 = k1 * q2;
+			p3 = k2 * q1;
+			p4 = k2 * p2;
+
+			return xp;
 		}
 
 		public float Poisition
